d30a0d0a4ba0bf452105ec16bc44685ec0d0aa65
MatDawit <mathewiyob@gmail.com>
2025-09-28 11:12:08 -0400

    render testing

--- frontend/src/pages/UploadFile.jsx
+++ frontend/src/pages/UploadFile.jsx
@@ -20,7 +20,7 @@ export default function UploadFile() {
       setLoading(true);
 
       try {
-          const response = await fetch("http://localhost:5000/api/upload", {
+          const response = await fetch("https://hackumbc2025.onrender.com/api/upload", {
               method: "POST",
               body: formData
           });
