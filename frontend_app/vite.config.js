b5abef94fbcd3a5ad06bedfea048f507760897ed
MatDawit <mathewiyob@gmail.com>
2025-10-01 01:32:58 -0400

    found fix in vite.config.js

--- frontend_app/vite.config.js
+++ frontend_app/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/hackUMBC2025/", // 👈 repo name again
+  base: "/SnapStudy/", // 👈 repo name again
 });
